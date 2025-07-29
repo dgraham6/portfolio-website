2c6a02efd7ad3b523c66704f08128ea254826653
Drake <digraham@umich.edu>
2025-07-29 18:54:30 -0400

    favicon

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("4c8b4bdaa76ba4af")
+/******/ 		__webpack_require__.h = () => ("c96482c214b3b58b")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
